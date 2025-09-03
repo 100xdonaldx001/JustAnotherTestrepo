d3c1627fa5fbfcc0c20dbcbbcd6e2cf752393317
Donald <daniel.b98@gmx.de>
2025-09-03 23:44:52 +0200

    Add nightlife activity with clubbing and bar hopping

--- renderers/activities.js
+++ renderers/activities.js
@@ -6,6 +6,7 @@ import { renderPets } from '../activities/pets.js';
 import { renderAdoption } from '../activities/adoption.js';
 import { renderCasino } from '../activities/casino.js';
 import { renderDoctor } from '../activities/doctor.js';
+import { renderNightlife } from '../activities/nightlife.js';
 
 const ACTIVITIES_CATEGORIES = {
   'Leisure & Lifestyle': [
@@ -63,6 +64,7 @@ const ACTIVITY_RENDERERS = {
   Adoption: () => openWindow('adoption', 'Adoption', renderAdoption),
   Lottery: () => openWindow('lottery', 'Lottery', renderLottery),
   Vacation: () => openWindow('vacation', 'Vacation', renderVacation),
+  Nightlife: () => openWindow('nightlife', 'Nightlife', renderNightlife),
   Pets: () => openWindow('pets', 'Pets', renderPets)
 };
 
